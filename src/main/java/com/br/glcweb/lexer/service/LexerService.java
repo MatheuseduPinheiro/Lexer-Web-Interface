8d4a6ae4de3b5120ff7cfd0afeccdbfbd3cd352d
Matheus Pinheiro <matheusedulimapinheiro@gmail.com>
2024-06-03 17:14:46 -0300

    Aproveitando a lógica do analisador léxico baseado no livro Compiladores Principios, Tecnicas e Ferramentas

--- src/main/java/com/br/glcweb/lexer/service/LexerService.java
+++ src/main/java/com/br/glcweb/lexer/service/LexerService.java
@@ -1,12 +1,32 @@
 package com.br.glcweb.lexer.service;
 
+import com.br.glcweb.lexer.Lexer;
+import com.br.glcweb.lexer.Tag;
+import com.br.glcweb.lexer.Token;
+
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.activemq.util.ByteArrayInputStream;
 import org.springframework.stereotype.Service;
 
 @Service
 public class LexerService {
 
     public String parseInput(String input) {
-        // Lógica de análise léxica aqui
-        return "Resultado da análise léxica para o input: " + input;
+        StringBuilder result = new StringBuilder();
+        try {
+            // Cria um Lexer com a entrada fornecida
+            InputStream inputStream = new ByteArrayInputStream(input.getBytes());
+            Lexer lexer = new Lexer(inputStream);
+            
+            // Faz a análise léxica do texto fornecido
+            Token token;
+            while ((token = lexer.scan()).tag != Tag.EOF) {
+                result.append(token.toString()).append("\n");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result.toString();
     }
 }
