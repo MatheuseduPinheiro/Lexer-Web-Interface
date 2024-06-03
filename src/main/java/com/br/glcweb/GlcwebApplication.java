047f4ee899209c26b3ec06ca4bb35a339d4878d9
Matheus Pinheiro <matheusedulimapinheiro@gmail.com>
2024-06-03 15:21:34 -0300

    Criação do template do projeto

---
+++ src/main/java/com/br/glcweb/GlcwebApplication.java
@@ -0,0 +1,13 @@
+package com.br.glcweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlcwebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlcwebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/glcweb/glcweb/GlcwebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.glcweb.glcweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GlcwebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
